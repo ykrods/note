ba232614ecac0ec5546196e07042a46923bc07af
ykrods <890082+ykrods@users.noreply.github.com>
2021-01-15 13:15:40 +0900

    供養衆

---
+++ src/kuyo/2020/SortedWithHeaderRecyclerViewAdapter.kt
@@ -0,0 +1,54 @@
+package xxx
+
+import android.support.v7.util.SortedList
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+abstract class SortedWithHeaderRecyclerViewAdapter <T, VH, HeaderVH>(
+        private var headerViewHolder :HeaderVH
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>()
+        where VH: RecyclerView.ViewHolder, HeaderVH :RecyclerView.ViewHolder {
+
+    companion object {
+        const val VIEW_TYPE_HEADER: Int = 0
+        const val VIEW_TYPE_LIST: Int = 1
+    }
+
+    abstract var sortedList: SortedList<T>
+
+    fun addItems(items :List<T>) {
+        sortedList.addAll(items)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (position == 0) {
+            return VIEW_TYPE_HEADER
+        }
+        return VIEW_TYPE_LIST
+    }
+
+    override fun getItemCount(): Int {
+        return sortedList.size() + 1
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType) {
+            VIEW_TYPE_HEADER -> headerViewHolder
+            else -> onCreateListItemViewHolder(parent)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (position == 0) {
+            @Suppress("UNCHECKED_CAST")
+            return onBindHeaderViewHolder(holder as HeaderVH, position)
+        }
+        @Suppress("UNCHECKED_CAST")
+        return onBindListItemViewHolder(holder as VH, position - 1)
+    }
+
+    abstract fun onCreateListItemViewHolder(parent: ViewGroup): VH
+
+    abstract fun onBindHeaderViewHolder(holder :HeaderVH, position :Int)
+    abstract fun onBindListItemViewHolder(holder :VH, position: Int)
+}
---
+++ src/kuyo/2021/aiohttp-in-thread.py
@@ -0,0 +1,73 @@
+"""
+裏で aiohttp (3.7.3で確認)のサーバを起動するようなのの試作
+
+* 参考: https://stackoverflow.com/questions/51610074/how-to-run-an-aiohttp-server-in-a-thread
+
+  * ``on_cleanup`` が正しく呼ばれるようにスレッドの外からサーバを止められるようにした
+
+* いい方法ではないっぽいので ``asyncio.gather`` とかで対応できるならそうした方がいい
+
+  * それが無理ならサブプロセスでやった方が良さそうな気がするので基本的にこいつの出番はないのではと思うがなんかあるかなぁ
+"""
+import asyncio
+import logging
+import threading
+
+from aiohttp import web
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+async def on_startup(app):
+    logger.debug("on_startup")
+
+async def on_cleanup(app):
+    logger.debug("on_cleanup")
+
+
+def aiohttp_runner():
+    async def say_hello(request):
+        return web.Response(text='Hello, world')
+
+    app = web.Application()
+    app.add_routes([
+        web.get('/', say_hello),
+    ])
+    app.on_startup.append(on_startup)
+    app.on_cleanup.append(on_cleanup)
+
+    runner = web.AppRunner(app)
+    return runner
+
+
+async def stop_server(loop, runner):
+    await runner.cleanup()
+    loop.stop()
+    loop.close()
+
+
+if __name__ == '__main__':
+    runner = aiohttp_runner()
+
+    loop = asyncio.get_event_loop()
+
+    loop.run_until_complete(runner.setup())
+    site = web.TCPSite(runner, 'localhost', 8080)
+    loop.run_until_complete(site.start())
+
+    t = threading.Thread(target=loop.run_forever)
+    t.start()
+
+    # loop が既に実行中なので別のループを使う
+    # subloop = asyncio.new_event_loop()
+    # suploop.run_until_complete(asyncio.sleep(1))
+
+    # t.join() して KeyboardInterrupt をとってもいいが、
+    # それだとスレッド使う意味がないので input() を使った例
+    while True:
+        msg = input('Type `quit` to stop server: ')
+        if msg == 'quit':
+            asyncio.run_coroutine_threadsafe(stop_server(loop, runner), loop)
+            break
