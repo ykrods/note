ad65b5c0e88403d925d62651c5ae98b1bd821f03
ykrods <890082+ykrods@users.noreply.github.com>
2021-01-18 13:57:24 +0900

    Update kuyo (aiohttp-in-thread.py)

--- src/kuyo/2021/aiohttp-in-thread.py
+++ src/kuyo/2021/aiohttp-in-thread.py
@@ -6,8 +6,6 @@
   * ``on_cleanup`` が正しく呼ばれるようにスレッドの外からサーバを止められるようにした
 
 * いい方法ではないっぽいので ``asyncio.gather`` とかで対応できるならそうした方がいい
-
-  * それが無理ならサブプロセスでやった方が良さそうな気がするので基本的にこいつの出番はないのではと思うがなんかあるかなぁ
 """
 import asyncio
 import logging
@@ -20,54 +18,50 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-async def on_startup(app):
-    logger.debug("on_startup")
+def aiohttp_app():
+    async def on_startup(app):
+        logger.debug("on_startup")
 
-async def on_cleanup(app):
-    logger.debug("on_cleanup")
+    async def on_cleanup(app):
+        logger.debug("on_cleanup")
 
-
-def aiohttp_runner():
     async def say_hello(request):
-        return web.Response(text='Hello, world')
+        return web.Response(text="Hello, world")
 
     app = web.Application()
     app.add_routes([
-        web.get('/', say_hello),
+        web.get("/", say_hello),
     ])
     app.on_startup.append(on_startup)
     app.on_cleanup.append(on_cleanup)
 
+    return app
+
+
+if __name__ == "__main__":
+    app = aiohttp_app()
     runner = web.AppRunner(app)
-    return runner
 
-
-async def stop_server(loop, runner):
-    await runner.cleanup()
-    loop.stop()
-    loop.close()
-
-
-if __name__ == '__main__':
-    runner = aiohttp_runner()
-
-    main_loop = asyncio.get_event_loop()
+    # スレッド用のループだが、サーバの起動・終了時はメインスレッドで使う
+    # (メインスレッドで async が使えない環境を想定)
     loop = asyncio.new_event_loop()
 
-    loop.run_until_complete(runner.setup())
-    site = web.TCPSite(runner, 'localhost', 8080)
-    loop.run_until_complete(site.start())
+    try:
+        loop.run_until_complete(runner.setup())
+        site = web.TCPSite(runner, "localhost", 8080)
+        loop.run_until_complete(site.start())
 
-    t = threading.Thread(target=loop.run_forever)
-    t.start()
+        t = threading.Thread(target=loop.run_forever)
+        t.start()
 
-    # loop が実行中なので別のループを使う必要がある
-    # main_loop.run_until_complete(asyncio.sleep(1))
-
-    # t.join() して KeyboardInterrupt をとってもいいが、
-    # それだとスレッド使う意味がないので input() を使った例
-    while True:
-        msg = input('Type `quit` to stop server: ')
-        if msg == 'quit':
-            asyncio.run_coroutine_threadsafe(stop_server(loop, runner), loop)
-            break
+        # t.join() して KeyboardInterrupt をとってもいいが、
+        # それだとスレッド使う意味がないので input() を使った例
+        while True:
+            msg = input("Type `quit` to stop server: ")
+            if msg == "quit":
+                loop.call_soon_threadsafe(loop.stop)
+                t.join()  # スレッドが止まるまで待つ
+                loop.run_until_complete(runner.cleanup())
+                break
+    finally:
+        loop.close()
